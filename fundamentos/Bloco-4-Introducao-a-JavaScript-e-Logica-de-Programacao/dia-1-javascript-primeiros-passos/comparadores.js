3beefebd952fdc303a9f3179a8d7aa808aa4a89c
Heron Baruch <heron_baruch@live.com>
2021-12-03 08:28:16 -0300

    exercicios de fixação primeiros passos

---
+++ fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/comparadores.js
@@ -0,0 +1,26 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+  const num1 = 3;
+  const num2 = 8;
+  if (num1 > num2) {
+    console.log(num1);
+  } else {
+    console.log(num2);
+  }
+
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+  const num1 = 4;
+  const num2 = 5;
+  const num3 = 6;
+  if ((num1 > num2) && (num1 > num3)) {
+    console.log(num1);
+  }else if (num2 > num3) {
+    console.log(num2);
+  }else{
+    console.log(num3);
+  }
+
+
+//Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+  
+
+
---
+++ fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/operadores.js
@@ -0,0 +1,31 @@
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+//adição (a+b)
+  let a = 2;
+  let b = 5;
+  let result = a + b;
+  console.log(result);
+
+//Subtração (a-b)
+  let a = 2;
+  let b = 5;
+  let result = a - b;
+  console.log(result);
+
+//Multiplicação (a*b)
+  let a = 2;
+  let b = 5;
+  let result = a * b;
+  console.log(result);
+
+//Divisão (a/b)
+  let a = 2;
+  let b = 5;
+  let result = a / b;
+  console.log(result);
+
+//Módulo (a%b)
+  let a = 2;
+  let b = 5;
+  let result = a % b;
+  console.log(result);
